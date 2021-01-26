8452ccdfd7d17244e3f8b9ca12df127e42cbd39d
wangchi <wangchi@ctrchina.cn>
2021-01-26 11:29:35 +0800

    conf新加mq配置

--- conf/config.go
+++ conf/config.go
@@ -7,6 +7,7 @@ import (
 // MyConfig 配置
 type MyConfig struct {
 	DataBases []DBConfig `json:"databases"`
+	MQs       []MQConfig `json:"mqs"`
 }
 
 // DBConfig 数据库详细配置
@@ -18,6 +19,15 @@ type DBConfig struct {
 	DBName string `json:"dbname"`
 	Driver string `json:"driver"`
 	Params string `json:"params"` //合并合法参数
+}
+
+// MQConfig 消息队列配置
+type MQConfig struct {
+	Host      string `json:"host"`
+	Port      int    `json:"port"`
+	User      string `json:"user"`
+	Pwd       string `json:"pwd"`
+	QueueName string `json:"queue"`
 }
 
 // ReadConfig 从file []byte 读取配置文件
--- conf/config_test.go
+++ conf/config_test.go
@@ -17,12 +17,15 @@ func TestReadConfig(t *testing.T) {
 		t.FailNow()
 	}
 	db1 := myconfig.DataBases[0]
-	if db1.Host != "KWPO-SERVER251\\KWPO251" && db1.Port != 1433 && db1.User != "test_user" && db1.Pwd != "@qq1234" && db1.DBName != "wc_test_db" && db1.Driver != "mssql" && db1.Params == "" {
+	if db1.Host != "KWPO-SERVER251\\KWPO251" && db1.Port != 1433 && db1.User != "test_user" && db1.Pwd != "@qq1234" && db1.DBName != "wc_test_db" && db1.Driver != "mssql" && db1.Params != "" {
 		t.Fail()
 	}
 	db2 := myconfig.DataBases[1]
-	if db2.Host != "KWPO-SERVER251" && db2.Port != 3306 && db2.User != "test_user" && db2.Pwd != "@qq1234" && db2.DBName != "wc_test_db" && db2.Driver != "mysql" && db2.Params == "timeout=30s" {
+	if db2.Host != "KWPO-SERVER251" && db2.Port != 3306 && db2.User != "test_user" && db2.Pwd != "@qq1234" && db2.DBName != "wc_test_db" && db2.Driver != "mysql" && db2.Params != "timeout=30s" {
 		t.Fail()
 	}
-
+	mq1 := myconfig.MQs[0]
+	if mq1.Host != "10.20.12.80" && mq1.Port != 5672 && mq1.User != "test" && mq1.Pwd != "XSojliSA" && mq1.QueueName != "test" {
+		t.Fail()
+	}
 }
