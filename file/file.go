0b16b09231825ec547136d746c7aa79884c7f5fc
wangchi <wangchi@ctrchina.cn>
2021-05-14 17:05:24 +0800

    新增file.ReadCsvWithInterface， 返回[][]interface{},方便使用sql.bulkcopy

--- file/file.go
+++ file/file.go
@@ -3,11 +3,17 @@ package file
 import (
 	"bytes"
 	"encoding/csv"
+	"errors"
+	"reflect"
+	"strconv"
 )
 
+type decoderFunc func([]byte) interface{}
+type decoderCache []decoderFunc
+
 type csvOptions struct {
-	comma   rune
-	comment rune
+	comma   rune // 默认","
+	comment rune // 默认"/n"
 }
 
 type csvOptionFn struct {
@@ -18,16 +24,17 @@ type CsvOption interface {
 	apply(*csvOptions)
 }
 
-// ReadCsv 读取csv文件内容，统一返回[][]string，如果需要特殊格式，自行转换
+// ReadCsv 读取csv文件内容，统一返回[][]string
 func ReadCsv(file []byte, opts ...CsvOption) ([][]string, error) {
 	return readCsv(file, opts...)
 }
 
 // trimBom 如果file是utf-8 bom 格式，将会转换成普通utf-8格式
-func trimBom(file []byte) {
+func trimBom(file []byte) []byte {
 	if file[0] == 0xef || file[1] == 0xbb || file[2] == 0xbf {
-		file = file[3:]
+		return file[3:]
 	}
+	return file
 }
 
 func readCsv(file []byte, opts ...CsvOption) ([][]string, error) {
@@ -35,7 +42,7 @@ func readCsv(file []byte, opts ...CsvOption) ([][]string, error) {
 	for _, o := range opts {
 		o.apply(opt)
 	}
-	trimBom(file)
+	file = trimBom(file)
 	b := bytes.NewReader(file)
 	r := csv.NewReader(b)
 	r.Comma = opt.comma
@@ -70,4 +77,165 @@ func defaultCsvOptions() *csvOptions {
 		comma:   ',',
 		comment: '\n',
 	}
+}
+
+// ReadCsvWithInterface 读取csv文件内容，返回[][]interface{},interface{}z中包含struct字段的真实类型
+// 方便使用mssql bulkcopy
+func ReadCsvWithInterface(file []byte, v interface{}, opts ...CsvOption) ([][]interface{}, error) {
+	return readCsvWithInterface(file, v, opts...)
+}
+
+func readCsvWithInterface(file []byte, v interface{}, opts ...CsvOption) ([][]interface{}, error) {
+	opt := defaultCsvOptions()
+	for _, o := range opts {
+		o.apply(opt)
+	}
+	file = trimBom(file)
+	rows := bytes.Split(file, []byte{byte(opt.comment)})
+	if len(rows) == 0 {
+		return [][]interface{}{}, nil
+	}
+	cache, err := scan(v)
+	if err != nil {
+		return nil, err
+	}
+
+	result := make([][]interface{}, len(rows))
+	for i := range rows {
+		row := make([]interface{}, len(cache))
+		cells := bytes.Split(rows[i], []byte{byte(opt.comma)})
+		for j := range cells {
+			row[j] = cache[j](cells[j])
+		}
+		result[i] = row
+	}
+	return result, nil
+}
+
+func scan(v interface{}) (decoderCache, error) {
+	t := reflect.TypeOf(v)
+	if t.Kind() != reflect.Struct {
+		return nil, errors.New("传入参数必须要struct类型")
+	}
+	num := t.NumField()
+	if num == 0 {
+		return nil, errors.New("传入参数必须要包含可访问字段")
+	}
+	cache := make([]decoderFunc, num)
+	for i := 0; i < num; i++ {
+		f := t.Field(i)
+		// interface{}中，不能只包含值，还需要有类型
+		// 从csv中读取出来的数据，都是文本形式，不是二进制形式，不可以使用binary包，结果会错误
+		switch f.Type.Kind() {
+		case reflect.Int:
+			cache[i] = decoderInt
+		case reflect.Int8:
+			cache[i] = decoderInt8
+		case reflect.Int16:
+			cache[i] = decoderInt16
+		case reflect.Int32:
+			cache[i] = decoderInt32
+		case reflect.Int64:
+			cache[i] = decoderInt64
+		case reflect.Uint:
+			cache[i] = decoderUint
+		case reflect.Uint8:
+			cache[i] = decoderUint8
+		case reflect.Uint16:
+			cache[i] = decoderUint16
+		case reflect.Uint32:
+			cache[i] = decoderUint32
+		case reflect.Uint64:
+			cache[i] = decoderUint64
+		case reflect.Float32:
+			cache[i] = decoderFloat32
+		case reflect.Float64:
+			cache[i] = decoderFloat64
+		case reflect.String:
+			cache[i] = decoderString
+		case reflect.Bool:
+			cache[i] = decoderBool
+		}
+	}
+	return cache, nil
+}
+
+func decoderInt(b []byte) interface{} {
+	return int(decoderInt64R(b))
+}
+
+func decoderInt8(b []byte) interface{} {
+	return int8(decoderInt64R(b))
+}
+
+func decoderInt16(b []byte) interface{} {
+	return int16(decoderInt64R(b))
+}
+
+func decoderInt32(b []byte) interface{} {
+	return int32(decoderInt64R(b))
+}
+func decoderInt64(b []byte) interface{} {
+	return decoderInt64R(b)
+}
+
+func decoderInt64R(b []byte) int64 {
+	i, err := strconv.ParseInt(string(b), 10, 64)
+	if err != nil {
+		panic("convert to int error ,check type")
+	}
+	return i
+}
+
+func decoderUint(b []byte) interface{} {
+	return uint(decoderUint64R(b))
+}
+
+func decoderUint8(b []byte) interface{} {
+	return uint8(decoderUint64R(b))
+}
+
+func decoderUint16(b []byte) interface{} {
+	return uint16(decoderUint64R(b))
+}
+
+func decoderUint32(b []byte) interface{} {
+	return uint32(decoderUint64R(b))
+}
+func decoderUint64(b []byte) interface{} {
+	return decoderUint64R(b)
+}
+
+func decoderUint64R(b []byte) uint64 {
+	i, err := strconv.ParseUint(string(b), 10, 64)
+	if err != nil {
+		panic("convert to int error ,check type")
+	}
+	return i
+}
+
+func decoderFloat32(b []byte) interface{} {
+	f, err := strconv.ParseFloat(string(b), 32)
+	if err != nil {
+		panic("convert to float error ,check type")
+	}
+	return float32(f)
+}
+
+func decoderFloat64(b []byte) interface{} {
+	s := string(b)
+	f, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		panic("convert to float error ,check type")
+	}
+	return f
+}
+
+func decoderString(b []byte) interface{} {
+	return string(b[:])
+}
+
+func decoderBool(b []byte) interface{} {
+	v, _ := strconv.ParseBool(string(b))
+	return v
 }
--- file/file_test.go
+++ file/file_test.go
@@ -1,7 +1,9 @@
 package file
 
 import (
+	"fmt"
 	"io/ioutil"
+	"math"
 	"testing"
 )
 
@@ -67,4 +69,112 @@ func TestReadCsvUTF8BOM(t *testing.T) {
 	if testResult1[4][0] != "五" || testResult1[4][1] != "哈哈我" {
 		t.Log("csv content error")
 	}
+}
+
+func TestReadCsvWithInterface(t *testing.T) {
+	type data struct {
+		Int     int
+		Int8    int8
+		Int16   int16
+		Int32   int32
+		Int64   int64
+		Uint    uint
+		Uint8   uint8
+		Uint16  uint16
+		Uint32  uint32
+		Uint64  uint64
+		Float32 float32
+		Float64 float64
+		String  string
+		Bool    bool
+	}
+	table := []data{
+		{
+			Int:     math.MaxInt64,
+			Int8:    math.MaxInt8,
+			Int16:   math.MaxInt16,
+			Int32:   math.MaxInt32,
+			Int64:   math.MaxInt64,
+			Uint:    math.MaxUint64,
+			Uint8:   math.MaxUint8,
+			Uint16:  math.MaxUint16,
+			Uint32:  math.MaxUint32,
+			Uint64:  math.MaxUint64,
+			Float32: float32(3.1415),
+			Float64: float64(3.1415926),
+			String:  "asdf",
+			Bool:    false,
+		},
+		{
+			Int:     math.MinInt64,
+			Int8:    math.MinInt8,
+			Int16:   math.MinInt16,
+			Int32:   math.MinInt32,
+			Int64:   math.MinInt64,
+			Uint:    0,
+			Uint8:   0,
+			Uint16:  0,
+			Uint32:  0,
+			Uint64:  0,
+			Float32: float32(-3.1415),
+			Float64: float64(-3.1415926),
+			String:  "",
+			Bool:    true,
+		},
+	}
+	for i := range table {
+		d := table[i]
+		s := fmt.Sprintf("%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%v,%v,%s,%t",
+			d.Int, d.Int8, d.Int16, d.Int32, d.Int64, d.Uint, d.Uint8, d.Uint16, d.Uint32, d.Uint64, d.Float32, d.Float64, d.String, d.Bool)
+		d1 := data{}
+		d2, err := readCsvWithInterface([]byte(s), d1)
+		if err != nil || len(d2) != 1 {
+			t.FailNow()
+		}
+		d3 := d2[0]
+		if d3[0] != d.Int {
+			t.Logf("%v:%v", d3[0], d.Int)
+		}
+		if d3[1] != d.Int8 {
+			t.Logf("%v:%v", d3[1], d.Int)
+		}
+		if d3[2] != d.Int16 {
+			t.Logf("%v:%v", d3[2], d.Int)
+		}
+		if d3[3] != d.Int32 {
+			t.Logf("%v:%v", d3[3], d.Int)
+		}
+		if d3[4] != d.Int64 {
+			t.Logf("%v:%v", d3[4], d.Int64)
+		}
+		if d3[5] != d.Uint {
+			t.Logf("%v:%v", d3[5], d.Uint)
+		}
+		if d3[6] != d.Uint8 {
+			t.Logf("%v:%v", d3[6], d.Uint8)
+		}
+		if d3[7] != d.Uint16 {
+			t.Logf("%v:%v", d3[7], d.Uint16)
+		}
+		if d3[8] != d.Uint32 {
+			t.Logf("%v:%v", d3[8], d.Uint32)
+		}
+		if d3[9] != d.Uint64 {
+			t.Logf("%v:%v", d3[9], d.Uint64)
+		}
+		if d3[10] != d.Float32 {
+			t.Logf("%v:%v", d3[10], d.Float32)
+		}
+		if d3[11] != d.Float64 {
+			t.Logf("%v:%v", d3[11], d.Float64)
+		}
+
+		if d3[12] != d.String {
+			t.Logf("%v:%v", d3[12], d.String)
+		}
+		if d3[13] != d.Bool {
+			t.Logf("%v:%v", d3[13], d.Bool)
+		}
+	}
+
 }
